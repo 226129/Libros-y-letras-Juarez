0aaef3a1842ca2a81a8e9566546cb640343b95c3
226129 <al226129@alumnos.uacj.mx>
2024-11-17 19:55:05 -0700

    Inicio completo

--- multimedia/apps/catalogo/models.py
+++ multimedia/apps/catalogo/models.py
@@ -36,7 +36,7 @@ class Libro(models.Model):
     precio = models.DecimalField(max_digits=6, decimal_places=2)
     fecha_publicacion = models.DateField()
     imagen = models.ImageField(
-        upload_to='/images/libros', null=True, blank=True)
+        upload_to='libros', null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
